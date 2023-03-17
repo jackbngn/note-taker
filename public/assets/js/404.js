74f879e7e9e44f35451fb6fc40cdfd440b04d0c1
Jack <jackngn83@gmail.comk>
2023-03-16 18:36:03 -0700

    done

--- public/assets/js/404.js
+++ public/assets/js/404.js
@@ -1,6 +1,8 @@
 const button = document.getElementById("home");
 
-button.addEventListener("click", () => {
-	console.log("it workss");
-	window.location.href = "../../index.html";
-});
+button.addEventListener("click", goBack);
+
+const goBack = (event) => {
+	event.preventDefault();
+	history.back();
+};
--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const path = require("path");
-const api = require("./route/index");
+const api = require("./routes/index.js");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use("/api", api);
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "/public/index.html"));
@@ -18,7 +19,11 @@ app.get("/", (req, res) => {
 app.get("/notes", (req, res) => {
 	res.sendFile(path.join(__dirname, "/public/assets/pages/notes.html"));
 });
+//404 page route if url is enter incorrect
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "/public/assets/pages/404.html"));
+});
 
 app.listen(PORT, () => {
-	console.log(`App listening at http://localhost:${PORT}`);
+	console.log("App listening at http://localhost:3001");
 });
